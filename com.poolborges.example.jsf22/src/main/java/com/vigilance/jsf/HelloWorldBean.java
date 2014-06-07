da98c942cff3a62953e886025c18fdd3f79e103f
Paulo Borges <poolborges@hotmail.com>
2014-06-07 17:21:46 +0100

    Examplo de JSF 2.2

---
+++ com.poolborges.example.jsf22/src/main/java/com/vigilance/jsf/HelloWorldBean.java
@@ -0,0 +1,31 @@
+package com.vigilance.jsf;
+
+import java.io.Serializable;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+@ManagedBean
+@RequestScoped
+public class HelloWorldBean implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    private String firstName;
+    private String lastName;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
